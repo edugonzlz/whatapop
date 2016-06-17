1690ec6de7576e25e978c45b825b7be95e69d8f4
Eduardo González <fonologico@gmail.com>
2016-06-17 23:48:50 +0200

    Incorporado boton que se pinta al clickar y se guarda en localStorage. Problemas: al arrancar la vista no se pinta el boton si es fav - No volvemos a la vista principial pinchando en whatapop y no se pinta la ruta de producto en la url

--- scripts/components/product-detail.js
+++ scripts/components/product-detail.js
@@ -24,9 +24,35 @@ angular.module("whatapop")
                     self.description = $sce.trustAsHtml(self.product.description);
 
                 });
+            var id = self.product.id;
+            self.fav = localStorage.getItem(id);
+            console.log(self.fav);
         };
         
         self.getImageUrl = ProductService.getImageUrl;
+        
+        self.makeFavorite = function () {
+            if (typeof(Storage) !== "undefined") {
+
+                var id = self.product.id;
+
+                //Recogemos el valor de nuestro id
+                var fav = localStorage.getItem(id);
+
+                if (fav === "fav"){
+                    //Eliminamos favorito
+                    localStorage.removeItem(id);
+                    self.fav = false;
+                    console.log(localStorage.getItem(id));
+                }
+                if (fav !== "fav"){
+                    //Si no es favorito lo guardamos como fav
+                    localStorage.setItem(id, "fav");
+                    self.fav = true;
+                    console.log(localStorage.getItem(id));
+                }
+            }
+        }
 
     }]
 });
