9141a285d7458aceee053572c8d5056b207fcde8
Eduardo González <fonologico@gmail.com>
2016-06-19 13:34:18 +0200

    ahora se muestra hace cuantos dias se publico el anuncio en la vista general

---
+++ scripts/filters/days-ago.js
@@ -0,0 +1,24 @@
+/**
+ * Created by Edu on 19/6/16.
+ */
+angular.module("whatapop")
+    .filter("DaysAgo", function () {
+        return function (date) {
+
+            var today = Date.now();
+            var dif = today - date;
+
+            var days = Math.floor(dif / (1000 * 60 * 60 * 24));
+            console.log("anuncio: ", date);
+            console.log("today: ", today);
+            console.log("days: ", days);
+
+            if (days <= 365){
+                var daysago = days + " días";
+            } else {
+                daysago = "mas de un año"
+            }
+
+            return daysago;
+        }
+    });
